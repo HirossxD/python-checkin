99cc0cd54623e0ce2b384d3a173d356be8212897
psakali <patrik.sakali@t-systems.com>
2026-01-29 14:53:26 +0300

    processes and source files

---
+++ skripty/processes/example-processes.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+echo "processes"
+
+echo "move process to background"
+sleep 10 &
+
+echo "pid of last background process"
+sleep 5 &
+echo "$!"
+ps -ef | grep 'sleep 5' | grep -v 'grep'
+
+
+echo "pid of running script"
+ps -ef | grep "$0" | grep -v 'grep'
+echo "$$"
+
+echo "move process to background and save pid"
+sleep 10 &
+pid="$!"
+
+
+echo "check background jobs"
+jobs
+
+
+echo "wait for background job to finish"
+#wait <pid>
+wait "$pid" && {
+  echo "sleep job with pid $pid finished"
+}
